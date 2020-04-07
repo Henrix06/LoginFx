e0075052f7f9b0b14a2fb99c11bf6a5cd46ccc15
Henrix B Moyo <48485373+Henrix06@users.noreply.github.com>
2020-04-07 19:21:25 +0200

    Add files via upload

--- LoginWindow/src/loginWindow/RegidterWindow.java
+++ LoginWindow/src/loginWindow/RegidterWindow.java
@@ -3,6 +3,7 @@ package loginWindow;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class RegidterWindow {
@@ -10,6 +11,7 @@ public class RegidterWindow {
     public TextField emailField;
     public Label label1;
     public Button regButton;
+    public PasswordField verifyPassword;
 
     public void Register(ActionEvent actionEvent) {
     }
